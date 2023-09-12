9758040ceb921997714f6d63ae2a80e5ded76561
j.senra <judson.nascimento@aperam.com>
2023-09-12 15:22:13 -0300

    antes de ir embora

--- estudo_cnn/tests/estudo_cnn/daf_service_class.py
+++ estudo_cnn/tests/estudo_cnn/daf_service_class.py
@@ -1,6 +1,7 @@
 import os
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
 class ReadFrameOpencv:
     def __init__(self):
@@ -24,7 +25,7 @@ class ReadFrameOpencv:
 
     def read_frames(self):
         if self.video_file is None:
-            raise ValueError("Video file path is not set.")
+            raise ValueError("Video file path is ncot set.")
         
         cap = cv2.VideoCapture(self.video_file)
         frames_list = []
@@ -64,23 +65,40 @@ class ReadFrameOpencv:
 
         return new_frame_list
 
-    def slicing_frames(self, frames, num_colunas=None, num_linhas=None):
+    def slicing_resize_frames(self, frames, num_colunas=None, num_linhas=None, save=False, hight=None, width=None):
         if num_colunas is None:
             raise ("Value error: num_columns can't be none")
         elif num_linhas is None:
             raise ("Value error: num_linhas can't be none")
+        if save:
+            for i, frame in enumerate(frames):
+                frame_id = f"sliced_frame_{i:04d}.jpg"
+                os.makedirs(f"saves_sliced", exist_ok=True)
 
-        for i, frame in enumerate(frames):
-            frame_id = f"sliced_frame_{i:04d}.jpg"
-            os.makedirs(f"saves_sliced", exist_ok=True)
+                partes = np.array_split(frame, num_colunas, axis=1)
+                for parte in partes:
+                    h_partes = np.array_split(parte, num_linhas, axis=0)
+                    partes = partes + h_partes
+
+                partes = partes[num_colunas:]
+                for i, parte in enumerate(partes):
+                    slice_id = f"slice_{i:03d}.jpg"
+                    caminho_arquivo = f"{frame_id}/{slice_id}"
+                    os.makedirs(f"saves_sliced/{caminho_arquivo}", exist_ok=True)
+                    frame_filename = os.path.join(f"saves_sliced/{caminho_arquivo}", f'slice_{i:04d}.jpg')
+                    cv2.imwrite(frame_filename, parte)
+
+        elif hight!=None:
+            for i, frame in enumerate(frames):
+                frame = plt.figure(figsize=(hight, width))
+                frame_id = f"sliced_frame_{i:04d}.jpg"
+                os.makedirs(f"saves_sliced", exist_ok=True)
 
             partes = np.array_split(frame, num_colunas, axis=1)
             for parte in partes:
                 h_partes = np.array_split(parte, num_linhas, axis=0)
                 partes = partes + h_partes
-            # partes = np.hsplit(frame, num_linhas)
-            # for parte in partes:
-            #     partes = np.vsplit(parte, num_colunas)
+
             partes = partes[num_colunas:]
             for i, parte in enumerate(partes):
                 slice_id = f"slice_{i:03d}.jpg"
@@ -88,3 +106,17 @@ class ReadFrameOpencv:
                 os.makedirs(f"saves_sliced/{caminho_arquivo}", exist_ok=True)
                 frame_filename = os.path.join(f"saves_sliced/{caminho_arquivo}", f'slice_{i:04d}.jpg')
                 cv2.imwrite(frame_filename, parte)
+
+        else:
+            for i, frame in enumerate(frames):
+                frame_id = f"sliced_frame_{i:04d}.jpg"
+                os.makedirs(f"saves_sliced", exist_ok=True)
+
+            partes = np.array_split(frame, num_colunas, axis=1)
+            for parte in partes:
+                h_partes = np.array_split(parte, num_linhas, axis=0)
+                partes = partes + h_partes
+
+            partes = partes[num_colunas:]
+            
+        return partes
