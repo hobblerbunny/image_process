cd41708c3cfdd5fe9fd746244948d093293e3c84
j.senra <judson.nascimento@aperam.com>
2023-09-22 13:47:04 -0300

    yesterday commits

--- estudo_cnn/tests/formatar_dataset/daf_service_class.py
+++ estudo_cnn/tests/formatar_dataset/daf_service_class.py
@@ -2,6 +2,7 @@ import os
 import cv2
 import numpy as np
 import pandas as pd
+import re
 
 """
 mais opções de slicing, tipo uma que dividi a imagem em
@@ -16,25 +17,8 @@ precisa criar um csv com as landmarks CHW(chann, height, width)
 
 class ReadFrameOpencv:
     def __init__(self):
-        self._video_file = None
         self._output_folder = None
-        self._image_folder = None
-
-    @property
-    def image_folder(self):
-        return self._image_folder
-
-    @image_folder.setter
-    def image_folder(self, path):
-        self._image_folder = path
-
-    @property
-    def video_file(self):
-        return self._video_file
-
-    @video_file.setter
-    def video_file(self, path):
-        self._video_file = file
+        self._read_frames = None
 
     @property
     def output_folder(self):
@@ -44,71 +28,52 @@ class ReadFrameOpencv:
     def output_folder(self, path):
         self._output_folder = path
 
+    @property
     def read_frames(self):
-        if self.video_file is None:
-            raise ValueError("Video file path is ncot set.")
-        
-        try:
-            cap = cv2.VideoCapture(self.video_file)
-        except:
-            raise ValueError("Not a video")
+        if isinstance(self._read_frames, bytes):
+            try:
+                cap = cv2.VideoCapture(self._read_frames)
+            except:
+                raise ValueError("Not a video")
+        elif isinstance(self._read_frames, str):
+            try:
+                file = os.listdir(self._read_frames)
+            except:
+                raise ValueError("Not images")
 
         try:
-            file = os.listdir(self.image_folder)
+            if cap:
+                frames_list = []
+                while True:
+                    ret, frame = cap.read()
+                    if not ret:
+                        break
+                    frames_list.append(frame)
+
+                cap.release()  # Importante liberar o recurso de captura após a leitura
+
         except:
-            raise ValueError("Not images")
-
-        if cap:
-            frames_list = []
-            while True:
-                ret, frame = cap.read()
-                if not ret:
-                    break
-                frames_list.append(frame)
-
-            cap.release()  # Importante liberar o recurso de captura após a leitura
-
-        elif file:
             frames_list = []
             for filename in file:
-                img = cv2.imread(os.path.join(self.image_folder, filename))
+                img = cv2.imread(os.path.join(self._read_frames, filename))
                 frames_list.append(img)
 
-        
-        
         return frames_list
+    
+    @read_frames.setter
+    def read_frames(self, frames):
+        self._read_frames = frames
 
     def save_frames(self, frames, all_in_one=False):
-        if isinstance(frames[0], np.ndarray):
-            if all_in_one:
-                raise ValueError("Output folder path is not set.")
-            
-            os.makedirs(f"{self.output_folder}", exist_ok=True)
-
-            for i, frame in enumerate(frames):
-                frame_filename = os.path.join(f"{self.output_folder}", f'frame_{i:04d}.jpg')
-                cv2.imwrite(frame_filename, frame)
-
-        elif isinstance(frames[0], np.ndarray):
-            if self.output_folder is None:
-                raise ValueError("Output folder path is not set.")
-            
-            os.makedirs(f"{self.output_folder}", exist_ok=True)
-
-            for i, frame in enumerate(frames):
-                frame_filename = os.path.join(f"{self.output_folder}", f'frame_{i:04d}.jpg')
-                cv2.imwrite(frame_filename, frame)
-        
-        elif isinstance(frames[0], list):
+        if isinstance(frames[0], list):
             for i, tuples in enumerate(frames):
-                tuple_id = f"sliced_frame_{i:04d}.jpg"
+                tuple_id = f"sliced_frame_{i:04d}"
                 os.makedirs(f"{self.output_folder}", exist_ok=True)
-
+                
                 if all_in_one:
                     for i, frame in enumerate(tuples):
                         slice_id = f"slice_{i:03d}.jpg"
-                        frame_filename = os.path.join(f"{self.output_folder}", slice_id)
-                        
+                        frame_filename = os.path.join(f"{self.output_folder}", f'{tuple_id}_{slice_id}')
                         cv2.imwrite(frame_filename, frame)
 
                 else:
@@ -116,12 +81,9 @@ class ReadFrameOpencv:
                             slice_id = f"slice_{i:03d}.jpg"
                             caminho_arquivo = f"{tuple_id}/{slice_id}"
                             os.makedirs(f"{self.output_folder}/{caminho_arquivo}", exist_ok=True)
-                            frame_filename = os.path.join(f"{self.output_folder}/{caminho_arquivo}", f'slice_{i:04d}.jpg')
+                            frame_filename = os.path.join(f"{self.output_folder}/{caminho_arquivo}", f'{tuple_id}_{slice_id}')
                             
                             cv2.imwrite(frame_filename, frame)
-
-
-
 
         elif isinstance(frames[0], np.ndarray):
             if self.output_folder is None:
@@ -132,7 +94,6 @@ class ReadFrameOpencv:
             for i, frame in enumerate(frames):
                 frame_filename = os.path.join(f"{self.output_folder}", f'frame_{i:04d}.jpg')
                 cv2.imwrite(frame_filename, frame)
-
 
     def matrix_color(self, frames, type):
         if type=="BGR>RGB":
@@ -177,7 +138,7 @@ class ReadFrameOpencv:
 
         return result
 
-    def slicing_frames(self, frames, num_colunas=None, num_linhas=None):
+    def slicing_frames(self, frames, num_linhas=None, num_colunas=None):
         if num_colunas is None:
             raise ("Value error: num_columns can't be none")
         elif num_linhas is None:
@@ -185,23 +146,25 @@ class ReadFrameOpencv:
 
         result = []
         for i, frame in enumerate(frames):
-            frame_id = f"sliced_frame_{i:04d}.jpg"
-            # os.makedirs(f"saves_sliced", exist_ok=True)
-
-            partes = np.array_split(frame, num_colunas, axis=1)
+            partes = np.array_split(frame, num_linhas, axis=0)
             for parte in partes:
-                h_partes = np.array_split(parte, num_linhas, axis=0)
+                h_partes = np.array_split(parte, num_colunas, axis=1)
                 partes = partes + h_partes
 
-            partes = partes[num_colunas:]
+            partes = partes[num_linhas:]
             tuple_partes = (partes)
             result.append(tuple_partes)
 
         return result
 
-    def landmarks(self, frames):
-        for i, frame in enumerate(frames): 
-            height, width, channels = self.frame.shape
-
+    # def landmarks(self, frames):
+    #     for i, frame in enumerate(frames): 
+    #         height, width, channels = self.frame.shape
+        
+    #     df = pd.DataFrame(columns=["name", "target", "class"])
+    #     for i, frame in enumerate(frames):
+    #         name = os.path.basename(frame)
 
         return frames
+
+
